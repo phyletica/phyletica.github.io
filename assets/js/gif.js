66bdc22f334a07c5f081b5980971a1c41eb163ac
Jamie Oaks <joaks1@gmail.com>
2015-08-26 23:50:59 -0700

    Updating gif control javascript code.
    Getting the click-to-play/click-to-stop behavior to work.

--- assets/js/gif.js
+++ assets/js/gif.js
@@ -9,13 +9,15 @@ $(document).ready(function() {
       var src = $(this).attr("src");
       $(this).attr("src", src.replace(/\.gif$/i, ".png"));
     });
-  $(".gif-click").on("click",
-    function() {
-      var src = $(this).attr("src");
-      $(this).attr("src", src.replace(/\.png$/i, ".gif"));
-    },
-    function() {
-      var src = $(this).attr("src");
-      $(this).attr("src", src.replace(/\.gif$/i, ".png"));
-    });
+  function handler1() {
+    var src = $(this).attr("src");
+    $(this).attr("src", src.replace(/\.png$/i, ".gif"));
+    $(this).one("click", handler2);
+  }
+  function handler2() {
+    var src = $(this).attr("src");
+    $(this).attr("src", src.replace(/\.gif$/i, ".png"));
+    $(this).one("click", handler1);
+  }
+  $(".gif-click").one("click", handler1);
 });
