31a3fc1f6c9ad87e09e9ffeb7b7bb4b7a072de70
Jamie Oaks <joaks1@gmail.com>
2015-09-02 21:17:47 -0700

    Fixing gif.js.
    Missed some calls when making function names more explicit. This
    broke the "gif-click" class behavior. This commit is the fix.

--- assets/js/gif.js
+++ assets/js/gif.js
@@ -1,23 +1,23 @@
 // Customize bourbon gif class (from http://codepen.io/CalebGrove/pen/bIsqy)
 $(document).ready(function() {
-  $(".gif-hover").hover(
-    function() {
-      var src = $(this).attr("src");
-      $(this).attr("src", src.replace(/\.png$/i, ".gif"));
-    },
-    function() {
-      var src = $(this).attr("src");
-      $(this).attr("src", src.replace(/\.gif$/i, ".png"));
-    });
-  function swap_to_gif() {
-    var src = $(this).attr("src");
-    $(this).attr("src", src.replace(/\.png$/i, ".gif"));
-    $(this).one("click", handler2);
-  }
-  function swap_to_png() {
-    var src = $(this).attr("src");
-    $(this).attr("src", src.replace(/\.gif$/i, ".png"));
-    $(this).one("click", handler1);
-  }
-  $(".gif-click").one("click", swap_to_gif);
+    $(".gif-hover").hover(
+        function() {
+            var src = $(this).attr("src");
+            $(this).attr("src", src.replace(/\.png$/i, ".gif"));
+        },
+        function() {
+            var src = $(this).attr("src");
+            $(this).attr("src", src.replace(/\.gif$/i, ".png"));
+        });
+    function swap_to_gif() {
+        var src = $(this).attr("src");
+        $(this).attr("src", src.replace(/\.png$/i, ".gif"));
+        $(this).one("click", swap_to_png);
+    }
+    function swap_to_png() {
+        var src = $(this).attr("src");
+        $(this).attr("src", src.replace(/\.gif$/i, ".png"));
+        $(this).one("click", swap_to_gif);
+    }
+    $(".gif-click").one("click", swap_to_gif);
 });
