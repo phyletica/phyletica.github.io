3ab529852630d56dfd261991293e37b81da4dbaa
Jamie R. Oaks <joaks1@gmail.com>
2025-08-26 14:37:28 -0500

    Updating update-bib.sh script and outputs.
    Having the 'update-bib.sh' script update both jro.bib and
    references.bib.
    
    Adding the updates of both files produced by the script.

--- bin/update-bib.sh
+++ bin/update-bib.sh
@@ -2,3 +2,5 @@
 
 # pybtex converts '~' to \\textasciitilde. Using sed below to convert back
 bibfilter.py ~/texmf/bibtex/bib/references.bib | grep -i -v 'author+an\|effort[[:blank:]]*=\|^[[:blank:]]*%\|keywords[[:blank:]]*=' | sed -e "s/\\\textasciitilde /~/g" > ../_bibliography/jro.bib
+
+cat ~/texmf/bibtex/bib/references.bib | grep -i -v 'author+an\|effort[[:blank:]]*=\|^[[:blank:]]*%\|keywords[[:blank:]]*=' > ../_bibliography/references.bib
